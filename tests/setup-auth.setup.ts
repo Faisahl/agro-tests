e2ae91e3d78e620c63899609b6686d11b2c575c6
Faisal Bokhari <Faisbokh@gmail.com>
2025-07-01 20:12:57 -0500

    setup change to nav instanc

--- tests/setup-auth.setup.ts
+++ tests/setup-auth.setup.ts
@@ -1,7 +1,7 @@
 import { test as setup, expect } from "@playwright/test";
 import { LoginPage } from "../pages/login.page";
-import { HomePage } from "../pages/home.page";
 import path from "path";
+import { NavBar } from "../pages/navbar.page";
 
 const authFile = path.join(__dirname, '../pwt/.auth/user.json');
 
@@ -10,7 +10,7 @@ setup('authenticate', async ({ page }) => {
     await signin.goto();
     await signin.signInUser('admin@kashtdar.com','d8T2[W0[4c-a');
     await expect(page).toHaveURL('/');
-    const home = new HomePage(page);
-    await expect(home.userIcon).toBeVisible();
+    const nav = new NavBar(page);
+    await expect(nav.userIconBtn).toBeVisible();
     await page.context().storageState({ path: authFile })
 })
