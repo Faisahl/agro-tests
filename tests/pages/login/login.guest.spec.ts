398d87cad0f14935be3700d029f6b926e339c688
Faisal Bokhari <Faisbokh@gmail.com>
2025-07-12 16:48:49 -0500

    test data updates

--- tests/pages/login/login.guest.spec.ts
+++ tests/pages/login/login.guest.spec.ts
@@ -1,47 +1,48 @@
-import {test as should} from "@playwright/test";
+import { test as should } from "@playwright/test";
 import { test, expect } from "../../../fixtures/e2e/goto-login";
 import { config } from "dotenv";
-// import { LoginPage } from "../../../pages/login.page";
-// import { NavBar } from "../../../pages/navbar.page";
+import { validTestUser as user } from "../../../test-data/users";
 
 config();
 
-const testUserEmail = process.env.AGRO_USER!;
-const testPass = process.env.AGRO_USER_PASS!;
-const testUserPhone = process.env.AGRO_USER_PHONE!;
-
-should.describe('@smoke - valid/invalid user sign in', () => {
-  test('tc_01 - login user with valid email credential', async ({ loginPage, page, navBar }) => {
-    // const signin = new LoginPage(page);
-    // await signin.goto();
-    // const nav = new NavBar(page);
-    await loginPage.signInUser(`${testUserEmail}`,testPass);
-    await expect(page).toHaveURL('/');
+should.describe("@smoke - valid/invalid user sign in", () => {
+  test("tc_01 - login user with valid email credential", async ({
+    loginPage,
+    page,
+    navBar,
+  }) => {
+    await loginPage.signInUser(user.email, user.password);
+    await expect(page).toHaveURL("/");
     await expect(navBar.userIconBtn).toBeVisible();
   });
 
-  test('tc_02 - login user with valid phone credential', async ({ loginPage, page, navBar }) => {
-    // const signin = new LoginPage(page);
-    // await signin.goto();
-    // const nav = new NavBar(page);
-    await loginPage.selectRadio('phone');
-    await loginPage.signInUser(`${testUserPhone}`,testPass);
-    await expect(page).toHaveURL('/');
+  test("tc_02 - login user with valid phone credential", async ({
+    loginPage,
+    page,
+    navBar,
+  }) => {
+    await loginPage.selectRadio("phone");
+    await loginPage.signInUser(user.phone, user.password);
+    await expect(page).toHaveURL("/");
     await expect(navBar.userIconBtn).toBeVisible();
   });
 
-  test('tc_03 - show invalid email login error (wrong password)', async ({ loginPage, page, navBar }) => {
-    // const signin = new LoginPage(page);
-    // await signin.goto();
-    await loginPage.signInUser(`${testUserEmail}`,'wrongpass');
+  test("tc_03 - show invalid email login error (wrong password)", async ({
+    loginPage,
+    // page,
+    // navBar,
+  }) => {
+    await loginPage.signInUser(user.email, "wrongpass");
     await expect(loginPage.loginError).toBeVisible();
   });
 });
 
-test('tc_04 - show invalid phone login error (wrong phone)', async ({ loginPage, page, navBar }) => {
-  // const signin = new LoginPage(page);
-  // await signin.goto();
-  await loginPage.selectRadio('phone');
-  await loginPage.signInUser('000010000',testPass);
+test("tc_04 - show invalid phone login error (wrong phone)", async ({
+  loginPage,
+  // page,
+  // navBar,
+}) => {
+  await loginPage.selectRadio("phone");
+  await loginPage.signInUser("000010000", user.password);
   await expect(loginPage.loginError).toBeVisible();
 });
